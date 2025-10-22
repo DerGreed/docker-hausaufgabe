8896f4365a6511c0a688baeb84c380ef3f61ccad
DerGreed <joel.hilberg@gmail.com>
2025-10-22 19:45:00 +0200

    added compose step, fixed index.js

--- visitor-counter/index.js
+++ visitor-counter/index.js
@@ -6,21 +6,19 @@ import redis from "redis";
 const app = express();
 const port = 8080;
 
-const client = redis.createClient({ host: "redis", port: 6379 });
-// const client = redis.createClient();
-client.on('error', (err) => console.log('Redis Client Error\n', err));
-// await client.connect();
+const client = redis.createClient({ url: "redis://redis:6379" });
+client.on("error", (err) => console.log("Redis Client Error\n", err));
+await client.connect();
 
 await client.set("visits", 0);
 
 app.get("/", async (req, res) => {
-  await client.get("visits", async (err, visits) => {
-    res.send(`
+  const visits = (await client.get("visits")) || "0";
+  res.send(`
       <h1>Anzahl der Besuche: ${visits}</h1>
       <p>Lade die Seite neu, um den Zähler zu erhöhen.</p>
-      `);
-    await client.set("visits", parseInt(visits) + 1);
-  });
+    `);
+  await client.set("visits", parseInt(visits) + 1);
 });
 app.listen(port, () => {
   console.log(`Besucherzähler-App lauscht auf Port ${port}!`);
