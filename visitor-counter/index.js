9c6923e8602317567425fa847207a4de9dbce357
DerGreed <joel.hilberg@gmail.com>
2025-10-22 18:29:37 +0200

    basic setup for visitor counter

---
+++ visitor-counter/index.js
@@ -0,0 +1,27 @@
+// const express = require("express");
+// const redis = require("redis");
+import express from "express";
+import redis from "redis";
+
+const app = express();
+const port = 8080;
+
+const client = redis.createClient({ host: "redis", port: 6379 });
+// const client = redis.createClient();
+client.on('error', (err) => console.log('Redis Client Error\n', err));
+// await client.connect();
+
+await client.set("visits", 0);
+
+app.get("/", async (req, res) => {
+  await client.get("visits", async (err, visits) => {
+    res.send(`
+      <h1>Anzahl der Besuche: ${visits}</h1>
+      <p>Lade die Seite neu, um den Zähler zu erhöhen.</p>
+      `);
+    await client.set("visits", parseInt(visits) + 1);
+  });
+});
+app.listen(port, () => {
+  console.log(`Besucherzähler-App lauscht auf Port ${port}!`);
+});
